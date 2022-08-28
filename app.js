32333a085807f4184eccc071aceae027185dd631
Md. Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-08-28 23:37:19 +0600

    Map Method

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
 //Example-01
-const arr = [2, 20, 13, 5, 6, 61, 23, 7]
-let arr1 = arr.filter((value)=>{
-    return value > 10
-})
-console.log(arr1);
+const arr = [4, 9, 16, 25];
+const newValue = arr.map(Math.sqrt);
+console.log(newValue);
 
 
-//Example-02
-const team = [
-    {
-        name:"Naim",
-        job:"Developer"
-    },
-    {
-        name:"Santo",
-        job:"UI Deginer"
-    },
-    {
-        name:"Banta",
-        job:"Developer"
-    },
-    {
-        name:"Kanta",
-        job:"App Developer"
-    },
-    {
-        name:"Malak",
-        job:"Youtuber"
-    }
-]
-let developer = team.filter((value)=>{
-    return value.job == 'Developer';
-})
-console.log(developer);
+
+
 
 
 
